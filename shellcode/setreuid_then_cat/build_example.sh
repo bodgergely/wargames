c4d9b50f0cb0cb6ecbe2bc10de6fac1d928fef13
Gergely Bod <bodgergely@hotmail.com>
2018-06-18 14:48:20 +0100

    fix -no-pie

--- shellcode/setreuid_then_cat/build_example.sh
+++ shellcode/setreuid_then_cat/build_example.sh
@@ -1,4 +1,4 @@
-gcc -m32 --no-pie -O0 -g -o example.o -c example.c
+gcc -m32 -no-pie -O0 -g -o example.o -c example.c
 gcc -m32 -O0 example.o setreuid_cat.o -o example
 sudo chown jozsi:geri ./example
 sudo chmod 4550 ./example
