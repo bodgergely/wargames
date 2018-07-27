8376c8b7bf374c005a079a5f1959d77a65096a03
Gergely Bod <bodgergely@hotmail.com>
2018-07-27 04:12:36 +0200

    softmmu

--- pwnable_kr/HackersSecret/softmmu/module/softmmu.c
+++ pwnable_kr/HackersSecret/softmmu/module/softmmu.c
@@ -2,18 +2,13 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 #include <linux/mm_types.h>
-//#include <asm-generic/pgtable.h>
-//#include <linux/slab.h>
-//#include <linux/vmalloc.h>
 #include <linux/mm.h>
 #include <linux/highmem.h>
-//#include <asm-generic/unistd.h>
-//#include <asm-generic/page.h>
 #include <linux/syscalls.h>
 #include <linux/proc_fs.h>
 
 typedef unsigned long long u64;
-typedef unsigned int u32;
+//typedef unsigned int u32;
 
 const char* module_name = "softmmu";
 
@@ -33,35 +28,54 @@ static const struct file_operations mmu_fops = {
 	.write		= mmu_write,
 };
 
+static int pte_callback(pte_t* pte, u32 addr, u32 end, struct mmu_walk* walk)
+{
+    printk("[softmmu] - pte callback for virt addr: %p: pte entry: %p, contains: %p, at virt addr: %p\n", addr, pte, *pte);
+}
 
-static u64 mmu_walk(u32 vaddr)
+static int page_walk(u32 vaddr)
+{
+    struct mm_struct* mm = current->mm;
+    struct mm_walk memwalk;
+    memwalk.mm = mm;
+    memwalk.pte_entry = &pte_callback;
+    return walk_page_range(vaddr, vaddr+4096, &memwalk);
+} 
+
+static u32 mmu_walk(u32 vaddr)
 {
     struct task_struct* proc = current;
     struct mm_struct* mm = proc->mm;
     pgd_t* pgd;
+    pud_t* pud;
     pmd_t* pmd;
     pte_t* ptep;
     pte_t  pte;
 
     pgd = pgd_offset(mm, vaddr);
-    if (pgd_none(*pgd) || pgd_bad(*pgd)) {
-        printk("[softmmu] - PGD not present of addr: %p", vaddr);
+    if (pgd_none_or_clear_bad(pgd)) {
+        printk("[softmmu] - PGD not present of addr: %p\n", vaddr);
         goto out;
     }
-    pmd = pmd_offset(pgd, vaddr);
-    if (pmd_none(*pmd) || pmd_bad(*pmd)) {
-        printk("[softmmu] - PMD not present of addr: %p", vaddr);
+	pud = pud_offset(pgd, vaddr);
+    if (pud_none_or_clear_bad(pud)) {
+        printk("[softmmu] - PUD not present of addr: %p\n", vaddr);
+        goto out;
+    }
+    pmd = pmd_offset(pud, vaddr);
+    if (pmd_none(*pmd)) {
+        printk("[softmmu] - PMD not present of addr: %p\n", vaddr);
         goto out;
     }
     ptep = pte_offset_map(pmd, vaddr);
-    if(!ptep) {
-        printk("[softmmu] - PTE not present of addr: %p", vaddr);
+    if(pte_none(*ptep)) {
+        printk("[softmmu] - PTE not present of addr: %p\n", vaddr);
         goto out;
     }
 
     pte = *ptep;
 
-    printk("Phys address of virt: %p is %llx", (void*)vaddr, pte.pte);
+    printk("Phys page of virt: %p is  at %p\n", (void*)vaddr, pte.pte);
     return pte.pte;
 out:
    return NULL; 
@@ -72,8 +86,16 @@ out:
 static ssize_t mmu_read(struct file *file, char __user *buf, size_t count, 
                         loff_t *ppos)
 {
-   mmu_walk(virt_addr);
-   return count;
+   u32 virt_page = virt_addr & 0xfffff000;
+   u32 phys_page = mmu_walk(virt_page);
+   u32 offset = virt_addr & 0xfff;
+   u32 phys_addr = phys_page + offset;
+   printk("[softmmu] - Virt addr: (%p) ---> Phys address (%p)\n", virt_addr, phys_addr);
+   if( (phys_addr & 0xfff) != offset) {
+       printk("[softmmu] - Phys offset not equal to virt offset(%x) - BUG\n", offset);
+   }
+   page_walk(virt_page);
+   return 4;
 }
 
 static ssize_t mmu_write(struct file *file, const char __user *buf,
@@ -88,22 +110,22 @@ static ssize_t mmu_write(struct file *file, const char __user *buf,
 static int mmuinit(void)
 {
     
-    printk("Loading x86 PAE MMU emulator");
-    printk("Write the virtual address to /proc/softmmu");
-    printk("You can obtain it',27h,'s physical address by reading /proc/%s", module_name);
-    printk("[+] i.e. echo -ne \\x00\\x80\\x04\\x08' > /proc/softmmu;");
-    printk("hexdump -C /proc/softmmu");
-    printk("[+] Let the kernel exploit begin :)");
+    printk("Loading x86 PAE MMU emulator\n");
+    printk("Write the virtual address to /proc/softmmu\n");
+    printk("You can obtain it's physical address by reading /proc/%s\n", module_name);
+    printk("[+] i.e. echo -ne \\x00\\x80\\x04\\x08' > /proc/softmmu;\n");
+    printk("hexdump -C /proc/softmmu\n");
+    printk("[+] Let the kernel exploit begin :)\n");
 
     proc_entry = create_proc_entry(module_name, 0x1b6, (struct proc_dir_entry*)NULL);
     if(!proc_entry) {
-        printk("Failed to create_proc_entry for softmmu!");
+        printk("Failed to create_proc_entry for softmmu!\n");
         return -1;
     }
     proc_entry->proc_fops = &mmu_fops;
 
-    printk("[softmmu] - mmu_read at: %p", mmu_read);
-    printk("[softmmu] - mmu_write at: %p", mmu_write);
+    printk("[softmmu] - mmu_read at: %p\n", mmu_read);
+    printk("[softmmu] - mmu_write at: %p\n", mmu_write);
 
     return 0;
 }
