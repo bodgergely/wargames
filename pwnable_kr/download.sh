fc73a91c5d592ede9dc7582ceea9f4c2ce73c362
Gergely Bod <bodgergely@hotmail.com>
2019-09-24 19:44:49 +0100

    python2 instead of miniconda2

--- pwnable_kr/download.sh
+++ pwnable_kr/download.sh
@@ -1,1 +1,1 @@
-~/miniconda2/bin/python ../scripts/download_file.py -s pwnable.kr -p 2222 -u $1 --password $2 -f $3 --local $4
+python2 ../scripts/download_file.py -s pwnable.kr -p 2222 -u $1 --password $2 -f $3 --local $4
--- pwnable_kr/download_softmmu.sh
+++ pwnable_kr/download_softmmu.sh
@@ -1,1 +1,1 @@
-~/miniconda2/bin/python ../scripts/download_file.py -s pwnable.kr -p 2222 -u softmmu --password "Congratz!! addr_limit looks quite IMPORTANT now... huh?" -f /softmmu.ko --local softmmu.ko
+python2 ../scripts/download_file.py -s pwnable.kr -p 2222 -u softmmu --password "Congratz!! addr_limit looks quite IMPORTANT now... huh?" -f /softmmu.ko --local softmmu.ko
