21240aae02ad90b7ab73cbc97f68556f489eb422
Gergely Bod <bodgergely@hotmail.com>
2019-12-17 18:35:58 +0100

    microblog: added followers table

--- tutorials/web_tech_tutorials/flask_tutorial/microblog/app/models.py
+++ tutorials/web_tech_tutorials/flask_tutorial/microblog/app/models.py
@@ -4,8 +4,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 from sqlalchemy.types import Integer, String, DateTime
-from sqlalchemy.schema import Column, ForeignKey
+from sqlalchemy.schema import Column, ForeignKey, Table
 
+"""
+Note that I am not declaring this table as a model, like I did for the users and posts tables. Since this is an auxiliary table that has no data other than the foreign keys, I created it without an associated model class.
+"""
+followers = db.Table('followers',
+    Column('follower_id', Integer, ForeignKey('user.id')),
+    Column('followed_id', Integer, ForeignKey('user.id'))
+)
 
 class User(UserMixin, db.Model):
     id = Column(Integer, primary_key=True)
@@ -15,6 +22,12 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = Column(String(140))
     last_seen = Column(DateTime, default=datetime.utcnow)
+
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
